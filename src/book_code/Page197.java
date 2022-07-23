7c6b8781c32654ed7d62ceab025518d64e5b42fd
jackinquantico <jackinquantico@naver.com>
2022-07-23 17:06:04 +0900

    fill array with random values

--- src/book_code/Page197.java
+++ src/book_code/Page197.java
@@ -2,11 +2,25 @@ package book_code;
 
 public class Page197 {
 	public static void main(String[] args) {
-		int[] score = {100,79,88,69,97,85,78,99,35};
+		// 길이가 10인 배열 생성
+		int[] score = new int[10];
 		
+		// 1부터 100 사이의 난수로 배열 채우기
+		for (int i=0; i < score.length; i++) {
+			int j = (int)(Math.random()*100)+1;
+			score[i] = j;
+			
+			// 배열 출력
+			System.out.print(score[i] + ",");
+		}
+		
+		System.out.println();
+		
+		// 변수에 배열의 첫번째 요소 저장
 		int max = score[0];
 		int min = score[0];
 		
+		// 배열 중 가장 큰 수와 작은 수로 변경
 		for (int i = 1; i < score.length; i++) {
 			if (score[i] > max)
 				max = score[i];
@@ -15,7 +29,7 @@ public class Page197 {
 		}
 		
 		System.out.println("배열요소 중 가장 큰 값은 "+max);
-		System.out.println("배열요소 중 가장 작은 값은 "+min);
+		System.out.println("배열요소 중 가장 작은 값은 "+ min);
 
 	}
 }
